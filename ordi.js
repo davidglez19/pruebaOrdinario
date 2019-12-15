4d59dce2770835f220ea9af0afc4bf850b36c46a
Amilcar <davidglez.ucol@gmail.com>
2019-12-15 09:44:15 -0600

    Iniciamos

---
+++ ordi.js
@@ -0,0 +1,23 @@
+var code = new Array(125,415,209);
+var description = new Array('Memoria UBS 16GB','Monitor 16 pulg','Mouse inalambrico');
+var price = new Array(140,2050,380);
+function buscar() {
+    var buscar = parseInt(document.getElementById("codigo").value);
+    var posicion = code.indexOf(buscar);
+    console.log(buscar);
+    if(posicion !=-1) {
+    document.getElementById("desc").value = description[posicion];
+    document.getElementById("price").value = price[posicion];
+    }else{
+        alert("Codigo no encontrado");
+    }
+}
+function Calcular() {
+    var precio = document.getElementById("price").value;
+    var cantidad = document.getElementById("cantidad").value;
+    var total = precio * cantidad;
+    document.getElementById("total").value = total;
+}
+function Almacenar() {
+
+}
